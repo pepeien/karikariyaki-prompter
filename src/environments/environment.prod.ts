74c21c5c294c12b5eefd8cf0b59eea00b90401bc
pepeien <erickfcaldeira@gmail.com>
2023-06-14 02:04:19 -0300

    Updated environment files and scripts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	production: true,
+	apiURI: 'https://189.59.189.174/karikariyaki/api',
+	wsURI: 'wss://189.59.189.174/karikariyaki/ws/prompter',
+	wsPath: '/karikariyaki/ws',
+};
