3bfa90528380083c75aef581dbc2d41c6a568c1e
pepeien <erickfcaldeira@gmail.com>
2023-06-17 15:34:10 -0300

    Updated prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiURI: 'https://189.59.189.174/karikariyaki/api',
-	wsURI: 'wss://189.59.189.174/karikariyaki/ws/prompter',
+	apiURI: 'https://api.ericodesu.com/karikariyaki/api',
+	wsURI: 'wss://api.ericodesu.com/karikariyaki/ws/prompter',
 	wsPath: '/karikariyaki/ws',
 };
